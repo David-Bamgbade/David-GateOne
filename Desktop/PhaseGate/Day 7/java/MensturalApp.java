6f4eb146c6ada30f215fff268b0e720918e5bb54
David-Bamgbade <davidbamgbade082@gmail.com>
2024-05-13 16:59:37 +0100

    update1

--- Desktop/PhaseGate/Day 7/java/MensturalApp.java
+++ Desktop/PhaseGate/Day 7/java/MensturalApp.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.time.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MensturalApp {
 public static void main(String[] args) {
@@ -7,19 +9,27 @@ public static void main(String[] args) {
 
 	Scanner scanner = new Scanner(System.in);
 
-	System.out.println("Date Your Last Period Started is DD-MM-YYYY");
+	System.out.println("The Date Your Last Period Started in YYYY-MM-DD");
 	String day = scanner.nextLine();
 
-	LocalDate date = LocalDate.parse(day);
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-mm-DD");
 
 	System.out.println("How Long Does Your Period Last");
 	int periodLast = scanner.nextInt();
-
+	
 	System.out.println("How Long Is your Menstural Cycle");
 	int cycle = scanner.nextInt();
 
-	Date period = new Date();
+	LocalDate date = LocalDate.parse(day);
+	
+	LocalDate nextPeriod = date.plusDays(28);
 
+	LocalDate endPeriod = nextPeriod.plusDays(periodLast);
+	
+	System.out.println("Your Next Period Starts From " + nextPeriod);
+	System.out.println("And It Ends At " + endPeriod);
+
+	
 
 
 	
