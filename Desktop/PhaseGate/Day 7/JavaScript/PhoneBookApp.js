1231319aa0756e085875fdb52c8302df08428438
David-Bamgbade <davidbamgbade082@gmail.com>
2024-05-16 07:35:33 +0100

    Last Update

---
+++ Desktop/PhaseGate/Day 7/JavaScript/PhoneBookApp.js
@@ -0,0 +1,37 @@
+let PhoneNumber = [];
+
+let FirstName = [];
+
+let LastName = [];
+
+let phoneBook = `
+	WELCOME TO PHONE BOOK
+
+	1-> Add contact
+
+	2-> Remove contact
+
+	3-> Find contact by phone number
+
+	4-> find contact by first name
+
+	5-> Find contact by last name
+
+	6-> Edit contact
+
+	7-> Back
+
+	
+	`;
+
+console.log(phoneBook);
+
+
+	
+
+
+
+
+
+
+
